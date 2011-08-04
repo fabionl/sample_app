6e26f4eff9d8d18b55dff2b7cdf078f99d52066b
Fabio Lima <fabionl@gmail.com>
2011-08-04 10:17:27 -0300

    Terminados os Exercicios (5.5) do tutorial. criando helper pra logo e testando os links das páginas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  # Retorna uma tag img contendo a Logo.png do aplicativo
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
   # Return a title on a per-page basis.
   def title
     base_title = "SampleApp"
@@ -9,6 +14,5 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
-
 
 end
